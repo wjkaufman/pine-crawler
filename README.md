d10fa08e517e417ed7291783d5cf2b59075cd70b
Will Kaufman <wjkhoops@gmail.com>
2017-07-28 20:51:22 -0600

    Save timetable data to json file

---
+++ README.md
@@ -0,0 +1,5 @@
+### To Do
+
+- get programs/departments [here](http://dartmouth.smartcatalogiq.com/en/current/orc/Departments-Programs-Undergraduate)
+- for each program/dept, go to div.sc-programlinks URL
+- for each course, get info, save it
--- index.js
+++ index.js
@@ -6,38 +6,48 @@ import cheerio from 'cheerio'
 
 const app = express()
 
+const timetableInfo = [
+    'term', 'CRN', 'subj', 'num', 'sec', 'title', 'text', 'xlist', 'period', 'room', 'building', 'instructor', 'WC', 'dist', 'lim', 'enrl', 'status', 'lrn-obj'
+]
+
 app.get('/', (req, res) => {
   console.log('got a request')
   res.send('Hello, world!')
+  
   let url = 'http://oracle-www.dartmouth.edu/dart/groucho/timetable.display_courses' +
   '?distribradio=alldistribs&depts=no_value&periods=no_value' +
   '&distribs=no_value&distribs_i=no_value&distribs_wc=no_value' +
   '&pmode=public&term=&levl=&fys=n&wrt=n&pe=n&review=n&crnl=no_value' +
   '&classyear=2008&searchtype=Subject+Area%28s%29&termradio=allterms' +
   '&terms=no_value&subjectradio=allsubjects&hoursradio=allhours&sortorder=dept'
-  
+
   let url2 = 'http://oracle-www.dartmouth.edu/dart/groucho/timetable.display_courses?distribradio=alldistribs&depts=no_value&periods=no_value&distribs=no_value&distribs_i=no_value&distribs_wc=no_value&pmode=public&term=&levl=&fys=n&wrt=n&pe=n&review=n&crnl=no_value&classyear=2008&searchtype=Subject+Area%28s%29&termradio=selectterms&terms=no_value&terms=201709&subjectradio=selectsubjects&depts=ENGS&hoursradio=allhours&sortorder=dept'
-  
-  request(url2, (err, res, html) => {
+
+  request(url, (err, res, html) => {
     if (err) {
       return console.error(err)
     }
-    console.log('No error!')
     var $ = cheerio.load(html)
+    
+    let courses = []
+    
     $('div.data-table table tbody tr').each(function(i, elem) {
-      console.log(`i is ${i}`)
-      // for each tr, which is one class
+    //   console.log(`i is ${i}`)
+      let course = {}
+      // for each tr, which is one course
       $(this).children('td').each(function(j, elem2) {
         // get td elements
-        console.log(`j is ${j}`)
-        console.log($(this).text())
+        if ($(this).text().trim().length > 0) {
+            course[timetableInfo[j]] = $(this).text().trim()
+        }
       })
+      
+      courses[i] = course
     })
-    
-    // for (let i = 0; i < data.length; i++) {
-    //   console.log('blah')
-    //   // console.log(data[i].children[0])
-    // }
+    // console.log(courses)
+    fs.writeFile('courses_timetable.json', JSON.stringify(courses), (err) => {
+        console.log('file written successfully')
+    })
   })
 })
 
