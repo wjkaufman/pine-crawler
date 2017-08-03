8fdb1e71d3cf1bd2ec6763681ff533cc0472bf08
Will Kaufman <wkaufman@appliedtrust.com>
2017-08-03 13:03:02 -0600

    Add jquery, bootstrap, angular, tether (probably not a good way to do things)

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import path from 'path'
 
-mongoose.connect('mongodb://localhost/pine-crawler')
+mongoose.connect('mongodb://localhost/pine-crawler', {
+  useMongoClient: true
+})
 
 const app = express()
 
-app.get('/', (req, res) => {
-  console.log('got a request')
-  res.send('Hello, world!')
-  
-  // timetable.saveTimetableInfo()
-  registrar.getAll()
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'pug')
+
+// app.get('/', (req, res) => {
+//   console.log('got a request')
+//   res.send('Hello, world!')
+//   
+//   // timetable.saveTimetableInfo()
+//   registrar.getAll()
+// })
+
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello there!' })
 })
 
 app.listen(8080, () => {
