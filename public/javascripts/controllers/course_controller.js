f145c036e93d579056a11f13971f451a5b7ea8fd
wjkaufman <wjkhoops@gmail.com>
2017-08-04 12:42:24 -0600

    Add distribs dropdown menu and sort by it

--- public/javascripts/controllers/course_controller.js
+++ public/javascripts/controllers/course_controller.js
@@ -16,12 +16,13 @@ angular.module('pineApp', [])
   .controller('courseController', function courseController($scope, $http) {
     $scope.test = 'hello, world!!'
     $scope.courses = []
+    $scope.distribs = ['W', 'NW', 'CI', 'ART', 'LIT', 'TMV', 'INT',
+        'SOC', 'QDS', 'SCI', 'SLA', 'TAS', 'TLA']
     
     // get list of subjects
     $http.get('/api/subjects')
     .then((res) => {
       $scope.subjects = res.data
-      console.log($scope.subjects)
     })
     
     // get course data
